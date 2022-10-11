be6799f6288134ec465d941583dbf18c457c9e6e
Pierre Poupin <pierrep@bam.tech>
2022-10-11 19:59:19 +0200

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier", "@typescript-eslint"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+};
